3404abfea70adfed7e6641c9eaefa707a81deb4d
sabilimaulana <spacysbl.learn@gmail.com>
2024-08-10 15:10:21 +0700

    feat: update greets

--- src/services/greets.ts
+++ src/services/greets.ts
@@ -4,7 +4,7 @@ import { db } from "../libs/db"
 import { s } from "../libs/db/schemas"
 
 export const getGreets = db
-  .select()
+  .select({ message: s.greets.message, createdAt: s.greets.createdAt })
   .from(s.greets)
   .orderBy(desc(s.greets.id))
   .prepare("prepared_greets")
